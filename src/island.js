8e3321b6339c44f4498d33cab1a7e9cf6f541d05
alekszych <aleks.zych@icloud.com>
2022-04-10 16:21:20 +0200

    zmiana kolorow i swiatla

--- src/island.js
+++ src/island.js
@@ -3,13 +3,15 @@ import {
     Mesh,
     MeshPhysicalMaterial,
     Float32BufferAttribute,
+    BufferAttribute,
     Color,
+    Vector3
 } from "three"
-import {convertVectorsToVertices, convertVerticesToVectors, getMatchingVertices} from "./globalFunctions"
+import {convertVectorsToVertices, convertVerticesToVectors, getMatchingVertices, getRandomInt} from "./globalFunctions"
 import {makeNoise2D} from "open-simplex-noise"
 
 export const createIsland = (scene, radius) => {
-    const mesh = new Mesh(new DodecahedronGeometry(radius, 6), new MeshPhysicalMaterial({vertexColors: true, flatShading: true}));
+    const mesh = new Mesh(new DodecahedronGeometry(radius, 2), new MeshPhysicalMaterial({vertexColors: true, flatShading: true}));
     let vertices = convertVerticesToVectors(mesh.geometry.attributes.position.array)
     let flatVertices = []
     vertices.forEach((verticle, i) => {
@@ -18,22 +20,23 @@ export const createIsland = (scene, radius) => {
             const y = -1
             const noise2D = makeNoise2D()
             matchingVertices.forEach(v => {
-                vertices[v].y = y + (0.4 * noise2D(vertices[v].x * 0.2, vertices[v].y * 0.3)) * (radius / 4)
+                vertices[v].y = y + (0.2 * noise2D(vertices[v].x * 2, vertices[v].y * 0.4)) * (radius / 3)
             })
             flatVertices = flatVertices.concat(matchingVertices)
         }
     })
     mesh.geometry.setAttribute('position', new Float32BufferAttribute(convertVectorsToVertices(vertices), 3))
 
-    const colors = []
-    const color = new Color()
-    vertices.forEach(() => {
-        color.set( ["#727272", "#557312"][Math.random() > 0.7 ? 0 : 1] )
-        colors.push( color.r, color.g, color.b )
-        colors.push( color.r, color.g, color.b )
-        colors.push( color.r, color.g, color.b )
+    const colors = [];
+    const color = new Color();
+    vertices.forEach(verticle => {
+        color.set( ["#727272", "#557312"][Math.random() > 0.7 ? 0 : 1] );
+
+        colors.push( color.r, color.g, color.b );
+        colors.push( color.r, color.g, color.b );
+        colors.push( color.r, color.g, color.b );
     })
-    mesh.geometry.setAttribute( 'color', new Float32BufferAttribute( colors, 3 ) )
+    mesh.geometry.setAttribute( 'color', new Float32BufferAttribute( colors, 3 ) );
     mesh.translateY(1)
     return mesh
 }
